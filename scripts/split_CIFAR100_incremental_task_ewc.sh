7a62b647d8c8062880125d537b2ebe705dcdd7f3
dsoselia <36576137+dsoselia@users.noreply.github.com>
2021-08-21 02:39:16 +0200

    cifar10 added

--- scripts/split_CIFAR100_incremental_task_ewc.sh
+++ scripts/split_CIFAR100_incremental_task_ewc.sh
@@ -1,6 +1,6 @@
 GPUID=$1
 OUTDIR=outputs/split_CIFAR100_incremental_task
-REPEAT=5
+REPEAT=2
 mkdir -p $OUTDIR
 python -u iBatchLearn.py --dataset CIFAR100 --train_aug --gpuid $GPUID --repeat $REPEAT --optimizer Adam    --force_out_dim 0 --first_split_size 20 --other_split_size 20 --schedule 80 120 160 --batch_size 128 --model_name WideResNet_28_2_cifar --model_type resnet                                              --lr 0.001 --offline_training         | tee ${OUTDIR}/Offline_adam.log
 python -u iBatchLearn.py --dataset CIFAR100 --train_aug --gpuid $GPUID --repeat $REPEAT --optimizer Adam    --force_out_dim 0 --first_split_size 20 --other_split_size 20 --schedule 80 120 160 --batch_size 128 --model_name WideResNet_28_2_cifar --model_type resnet                                              --lr 0.001                            | tee ${OUTDIR}/Adam.log
